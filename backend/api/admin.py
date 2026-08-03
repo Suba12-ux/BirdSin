c8f72be9b0e312986c8fe4547dc60a6e98f820db
suba <aasdfgh71@mail.ru>
2026-08-04 01:51:27 +0300

    added new fields for admin

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -8,16 +8,24 @@ from .models import User
 class UserAdmin(BaseUserAdmin):
     """Админ-панель для управления пользователями."""
 
-    ADDITIONAL_USER_FIELDS = (
+        ADDITIONAL_USER_FIELDS = (
         (
             None,
             {
-                'fields': ('avatar',)
+                'fields': ('avatar', 'is_developer')
             }
         ),
     )
     fieldsets = BaseUserAdmin.fieldsets + ADDITIONAL_USER_FIELDS
     search_fields = ('username', 'email',)
-    list_display = ('username', 'email', 'is_staff', 'is_active')
-    list_filter = ('is_staff', 'is_active')
+    list_display = (
+        'username', 'email',
+        'is_developer', 'is_staff',
+        'is_active'
+    )
+    list_filter = (
+        'is_staff',
+        'is_active',
+        'is_developer'
+    )
     ordering = ('username',)
